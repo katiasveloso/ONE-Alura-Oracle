05b148de4380ac4d4865e202fe8c8f7c70aceef0
katia <katisv@gmail.com>
2024-06-23 19:58:30 -0300

    Atualização

--- logica-js-projeto_inicial1/app.js
+++ logica-js-projeto_inicial1/app.js
@@ -1,11 +1,11 @@
 alert('Boas vindas ao Jodo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 7;
 console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 //se chute for igual ao número secretos
 if (chute == numeroSecreto) {
-  alert('Isso ai! Você descobriu o número secreto(5)');
+  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}` );
 }else {
     alert('Você errou :(')
 }
