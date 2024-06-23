602330c096dc91efdf0bd65bb3c75a1a5f4264db
katia <katisv@gmail.com>
2024-06-23 19:48:24 -0300

    Proejto

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao Jodo do número secreto');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10');
+
+//se chute for igual ao número secretos
+if (chute == numeroSecreto) {
+  alert('Isso ai! Você descobriu o número secreto(5)');
+}else {
+    alert('Você errou :(');
+}
