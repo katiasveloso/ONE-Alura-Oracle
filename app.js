3db929cbdbe38cea9e909ed99ef6880aa83742e5
katia <katisv@gmail.com>
2024-06-23 19:49:24 -0300

    projeto-js

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
   alert('Isso ai! Você descobriu o número secreto(5)');
 }else {
-    alert('Você errou :(');
+    alert('Você errou :(')
 }
