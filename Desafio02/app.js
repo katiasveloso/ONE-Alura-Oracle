a253a9ae18615c68a2efa057a6890cdf7d0aec00
katia <katisv@gmail.com>
2024-06-24 20:51:13 -0300

    Desafio

---
+++ Desafio02/app.js
@@ -0,0 +1,9 @@
+let Numero = prompt("Digite um número:");
+
+if (Numero > 0){
+    alert('Número Positivo')
+}else if (Numero < 0){
+        alert('Número negativo')
+    }
+
+
---
+++ logica-js-projeto_inicial1/Desafio01/app.js
@@ -0,0 +1,12 @@
+// Solicitar ao usuário o dia da semana
+let diaDaSemana = prompt('Qual o dia da semana?');
+
+
+// Verificar se o dia é sábado ou domingo
+if (diaDaSemana  == "Domingo" || diaDaSemana == "Sábado" ){
+    alert("Bom Final de Semana!!")
+   
+}else{
+    alert("Boa Semana!")
+    
+}
